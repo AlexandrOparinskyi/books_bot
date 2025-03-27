31d911c4c3c13293a70239fb04ec1e88beb6e7ab
Александр <opar1297@mail.ru>
2025-03-27 18:05:57 +0300

    Init commit

---
+++ handlers/user_handlers.py
@@ -0,0 +1,17 @@
+from aiogram import Router
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+from lexicons.lexicons_user_handlers import LEXICONS_USER_RU
+
+user_router = Router()
+
+
+@user_router.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(LEXICONS_USER_RU[message.text])
+
+
+@user_router.message(Command(commands="help"))
+async def process_help_command(message: Message):
+    await message.answer(LEXICONS_USER_RU[message.text])
---
+++ main.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config import load_config
+from handlers.user_handlers import user_router
+
+
+async def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    config = load_config()
+
+    bot: Bot = Bot(token=config.tg_bot.token,
+                   default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp: Dispatcher = Dispatcher()
+
+    dp.include_router(user_router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
