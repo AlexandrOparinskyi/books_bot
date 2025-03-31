b496c960c365fcf6473bf6df9f2aae1af137de46
alex <opar1297@mail.ru>
2025-03-31 14:12:01 +0300

    Исправление ошибок

--- handlers/profile_handlers.py
+++ handlers/profile_handlers.py
@@ -2,6 +2,7 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
+from lexicons.lexicon_profile_ru import LEXICON_PROFILE_RU
 from services.database_services import get_user_by_id
 
 profile_router = Router()
@@ -10,6 +11,10 @@ profile_router = Router()
 @profile_router.message(Command(commands="profile"))
 async def get_profile(message: Message):
     user = await get_user_by_id(message.from_user.id)
+    if user is None:
+        await message.answer(LEXICON_PROFILE_RU["user_not_exists"])
+        return
+
     text = (f"{user.name}\n"
             f"{user.surname}\n"
             f"{user.user_point[0].points} очков\n")
